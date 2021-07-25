0228cfd17f2e64e9d82c16090cda58007c531aca
Clay Curtis <jccurtis@presidio.com>
2021-07-25 11:43:35 -0400

    physical diagram in progress

--- net_diagram.py
+++ net_diagram.py
@@ -62,3 +62,69 @@ with Diagram("Logical Topology", show=False):
     [vrf101, vrf102, vrf103] - core_rtr
     core_rtr - dot1q - fw - rtr - ipv4 - plane_kame
     rtr - red_dashed_line - ipv6 - v6_traffic - dancing_kame
+
+with Diagram("Physical Topology", show=False, direction="TB"):
+
+    # Devices
+
+    with Cluster("Frontier FiOS PED"):
+        fios_intf_fiber1 = Custom("fiber term", f"{ICONPATH}/sq_interface.png")
+    with Cluster(nd.devices.fios_ont.hostname):
+        fios_intf_fiber2 = Custom("fiber term", f"{ICONPATH}/sq_interface.png")
+        fios_intf_1 = Custom("1", f"{ICONPATH}/sq_interface.png")
+    with Cluster(nd.devices.edge.hostname):
+        edge_intf_eth4 = Custom("eth4", f"{ICONPATH}/sq_interface.png")
+        edge_intf_eth3 = Custom("eth3", f"{ICONPATH}/sq_interface.png")
+        edge_intf_eth2 = Custom("eth2", f"{ICONPATH}/sq_interface.png")
+        edge_intf_eth1 = Custom("eth1", f"{ICONPATH}/sq_interface.png")
+        edge_intf_eth0 = Custom("eth0", f"{ICONPATH}/sq_interface.png")
+    with Cluster(nd.devices.palo_fw.hostname):
+        with Cluster("Management"):
+            fw_intf_mgt = Custom("MGT", f"{ICONPATH}/sq_interface.png")
+            fw_intf_con = Custom("CON", f"{ICONPATH}/sq_interface.png")
+        with Cluster("In-Band"):
+            fw_intf_8 = Custom("8", f"{ICONPATH}/sq_interface.png")
+            fw_intf_6 = Custom("6", f"{ICONPATH}/sq_interface.png")
+            fw_intf_4 = Custom("4", f"{ICONPATH}/sq_interface.png")
+            fw_intf_2 = Custom("2", f"{ICONPATH}/sq_interface.png")
+            fw_intf_7 = Custom("7", f"{ICONPATH}/sq_interface.png")
+            fw_intf_5 = Custom("5", f"{ICONPATH}/sq_interface.png")
+            fw_intf_3 = Custom("3", f"{ICONPATH}/sq_interface.png")
+            fw_intf_1 = Custom("1", f"{ICONPATH}/sq_interface.png")
+
+    with Cluster(nd.devices.core_rtr.hostname):
+        with Cluster("Management"):
+            core_rtr_intf_mgt = Custom("MGT", f"{ICONPATH}/sq_interface.png")
+            core_rtr_intf_con = Custom("CON", f"{ICONPATH}/sq_interface.png")
+        with Cluster("In-Band"):
+            core_rtr_intf_1 = Custom("1", f"{ICONPATH}/sq_interface.png")
+            core_rtr_intf_2 = Custom("2", f"{ICONPATH}/sq_interface.png")
+            core_rtr_intf_3 = Custom("3", f"{ICONPATH}/sq_interface.png")
+            core_rtr_intf_4 = Custom("4", f"{ICONPATH}/sq_interface.png")
+            core_rtr_intf_5 = Custom("5", f"{ICONPATH}/sq_interface.png")
+            core_rtr_intf_6 = Custom("6", f"{ICONPATH}/sq_interface.png")
+            core_rtr_intf_7 = Custom("7", f"{ICONPATH}/sq_interface.png")
+            core_rtr_intf_8 = Custom("8", f"{ICONPATH}/sq_interface.png")
+
+        # Workaround to get the interfaces to stack
+        fw_intf_1 - Edge(color="#EBF3E7") - fw_intf_2
+        fw_intf_3 - Edge(color="#EBF3E7") - fw_intf_4
+        fw_intf_5 - Edge(color="#EBF3E7") - fw_intf_6
+        fw_intf_7 - Edge(color="#EBF3E7") - fw_intf_8
+        fw_intf_mgt - Edge(color="#EBF3E7") - fw_intf_con
+        core_rtr_intf_7 - Edge(color="#EBF3E7") - core_rtr_intf_8
+        core_rtr_intf_5 - Edge(color="#EBF3E7") - core_rtr_intf_6
+        core_rtr_intf_3 - Edge(color="#EBF3E7") - core_rtr_intf_4
+        core_rtr_intf_1 - Edge(color="#EBF3E7") - core_rtr_intf_2
+
+    # Lines
+    fiber_cable = Edge(color="orange", style="bold")
+    # Connections
+    # Fiber from street to ONT
+    fios_intf_fiber1 - fiber_cable - fios_intf_fiber2
+    # Handoff from ONT to edge router
+    fios_intf_1 - edge_intf_eth2
+    # Edgerouter to Palo FW Connections
+    fw_intf_1 - edge_intf_eth1
+    # Palo to Core Router
+    fw_intf_8 - core_rtr_intf_1
